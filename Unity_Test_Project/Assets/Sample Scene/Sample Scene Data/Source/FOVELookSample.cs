a68b1ebd03251d106c30a41bcbba8f4d4a3a7d11
devriesewouter89 <devriese.wouter@gmail.com>
2017-09-28 10:08:36 +0200

    updated version with object recognition

--- Unity_Test_Project/Assets/Sample Scene/Sample Scene Data/Source/FOVELookSample.cs
+++ Unity_Test_Project/Assets/Sample Scene/Sample Scene Data/Source/FOVELookSample.cs
@@ -24,7 +24,7 @@ public class FOVELookSample : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+		//sendMsg (my_collider);
         if (FoveInterface.IsLookingAtCollider(my_collider))
 //send osc message mycollider.position
 		{	
@@ -56,8 +56,31 @@ public class FOVELookSample : MonoBehaviour {
 	{
 		OscMessage message = new OscMessage();
 
-		message.address = "/UpdateXYZ";
+		message.address = "/sound/" + my_collider.gameObject.name.Substring(0,4);//"/UpdateXYZ";
 		//Debug.Log ("COLLIDER:" +my_collider.transform.position.x+" "+my_collider.transform.position.y+" "+my_collider.transform.position.z);
+		//Decide which object is my_collider and thus give it an integer to choose correct soundfile
+		//int i=0;
+		//Debug.Log (my_collider.gameObject.name);
+		/*switch (my_collider.gameObject.name) {
+		case "head":
+			Debug.Log ("HOOFD");
+			i = 0;
+			break;
+		case "ball":
+			i = 1;
+			break;
+		case "rocket":
+			i = 2;
+			break;
+		
+		default:
+			i = 3;
+			Debug.Log ("een gewone doos");
+			break;
+		}*/
+		//Debug.Log("")
+		//message.values.Add (my_collider.gameObject.name);
+		//Add XYZ values of my_collider
 		message.values.Add(my_collider.transform.position.x);
 		message.values.Add(my_collider.transform.position.y);
 		message.values.Add(my_collider.transform.position.z);
